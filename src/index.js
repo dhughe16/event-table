d1ad915ed2fed83b359063ce66eb168615d71a8d
Darcy Hughes <dhughe16@asu.edu>
2018-11-13 13:33:46 -0700

    Move files and deleted container directory.

--- src/index.js
+++ src/index.js
@@ -5,5 +5,5 @@
 * Desc: This file imports elements to be used in the index.html file.
  */
 
-import BodyContainer from "./js/components/container/BodyContainer"
+import BodyContainer from "./js/components/BodyContainer"
 import "./styles/styles.css";
